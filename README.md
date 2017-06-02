2fb5cf1e29abe5a7d63177548934ab2c505f90c6
Michael Novotny <manovotny@gmail.com>
2017-06-02 15:30:49 -0500

    Adds test site.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # client-side-render-and-seo
-A client side rendered React site to test how seo does.
+
+> A client side rendered React site to test how seo does.
+
+Created using [create-react-app](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () =>
+    <section>
+        <header className="header">
+            <img src={logo} className="logo" alt="logo"/>
+            <h1>Welcome to a client side rendered React site! Let's see how seo does...</h1>
+        </header>
+        <nav className="nav">
+            <a href="/" title="Home">Home</a>
+            <a href="/about.html" title="About">About</a>
+            <a href="/contact.html" title="Contact">Contact</a>
+            <a href="https://google.com" title="Google">Google</a>
+        </nav>
+        <main className="main">
+            <p className="intro">
+                Angel investor social media startup alpha disruptive. Entrepreneur gen-z assets infrastructure
+                startup accelerator. IPad first mover advantage low hanging fruit prototype. Product management
+                bootstrapping direct mailing. Long tail twitter monetization mass market burn rate iPhone startup
+                innovator business-to-consumer learning curve crowdsource. Learning curve series A financing
+                customer business-to-business success agile development. Partner network pitch traction low hanging
+                fruit beta focus product management handshake virality interaction design seed money conversion
+                paradigm shift. Non-disclosure agreement facebook vesting period paradigm shift creative virality
+                lean startup advisor social proof rockstar social media partnership business-to-business. Ownership
+                ramen pivot low hanging fruit user experience iteration responsive web design churn rate. IPad
+                business plan interaction design niche market customer bootstrapping venture social proof alpha
+                equity user experience scrum project.
+            </p>
+            <p className="intro">
+                Facebook user experience social proof supply chain product management infrastructure conversion
+                responsive web design scrum project ramen social media disruptive success. Metrics sales android
+                channels responsive web design business model canvas. Infographic user experience series A financing
+                niche market direct mailing accelerator first mover advantage seed round. Handshake technology
+                market paradigm shift analytics incubator network effects bandwidth virality rockstar early
+                adopters. Bootstrapping social media alpha funding leverage beta metrics bandwidth non-disclosure
+                agreement early adopters ownership network effects channels. Branding vesting period crowdsource
+                twitter infographic customer social media churn rate beta low hanging fruit non-disclosure agreement
+                learning curve venture backing. Ecosystem seed round value proposition equity. Crowdsource
+                disruptive termsheet low hanging fruit monetization venture deployment prototype accelerator angel
+                investor traction. Handshake founders direct mailing user experience series A financing ecosystem
+                traction freemium. Monetization network effects strategy.
+            </p>
+            <p className="intro">
+                Freemium release founders entrepreneur social proof MVP research & development influencer assets.
+                Pivot vesting period virality agile development client responsive web design facebook A/B testing.
+                Social proof focus twitter bandwidth channels creative vesting period handshake launch party
+                bootstrapping. Bandwidth business-to-business release mass market advisor. Termsheet infographic
+                paradigm shift. Assets venture learning curve funding business-to-consumer growth hacking angel
+                investor deployment. Non-disclosure agreement technology gamification influencer startup marketing
+                deployment. Leverage sales infographic freemium social proof supply chain network effects creative.
+                Market deployment angel investor non-disclosure agreement twitter early adopters gen-z ecosystem.
+                Conversion disruptive seed round low hanging fruit channels monetization investor product management
+                strategy angel investor virality hackathon.
+            </p>
+            <p className="intro">
+                Startup entrepreneur user experience burn rate interaction design backing. Partnership pivot funding
+                client startup. Early adopters direct mailing burn rate user experience advisor pivot gen-z sales
+                technology conversion facebook validation. Business-to-consumer return on investment focus client.
+                Mass market business-to-business stock venture iPhone A/B testing. Advisor social media paradigm
+                shift agile development. Strategy investor mass market backing alpha value proposition. Stealth
+                influencer android supply chain. Partner network return on investment freemium. A/B testing series A
+                financing angel investor partnership traction stealth iteration social media MVP assets launch party
+                metrics paradigm shift.
+            </p>
+            <p className="intro">
+                Metrics lean startup non-disclosure agreement hackathon client return on investment value
+                proposition investor infographic business-to-consumer paradigm shift learning curve growth hacking
+                churn rate. Handshake equity accelerator agile development low hanging fruit traction funding
+                incubator A/B testing beta. Sales marketing customer disruptive analytics market partnership
+                ownership growth hacking. Business-to-consumer freemium startup launch party user experience value
+                proposition partnership. Release social proof early adopters stock. Handshake disruptive holy grail
+                interaction design MVP ownership mass market value proposition. Paradigm shift disruptive channels
+                assets user experience research & development entrepreneur seed round bandwidth backing stealth
+                marketing. Low hanging fruit backing paradigm shift hackathon. Business-to-business paradigm shift
+                growth hacking market traction. Infrastructure entrepreneur iteration.
+            </p>
+        </main>
+    </section>;
+
+export default App;
